2a2925df0bb013c456d254191b110a5e58543a99
Farhan Farooq <farhan.farooq@live.com>
2020-08-23 12:49:46 +0500

    entry create, read, update routes

--- src/services/mirage/routes/diary.ts
+++ src/services/mirage/routes/diary.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "miragejs";
-import { User, Diary } from "../../../interface";
+import { User, Diary, Entry } from "../../../interface";
 import { handleErrors } from "../server";
+
+/* DIARY ROUTES */
 
 // Create diary
 export const create = (
@@ -68,3 +70,77 @@ export const updateDiary = (schema: any, req: Request): Diary | Response => {
     return handleErrors(error, "Failed to update Diary.");
   }
 };
+
+/* END DIARY ROUTES */
+
+/* ENTRY ROUTES */
+
+// Add an entry
+export const addEntry = (
+  schema: any,
+  req: Request
+): { diary: Diary; entry: Entry } | Response => {
+  try {
+    // Find diary by params
+    const diary = schema.diaries.find(req.params.id);
+
+    const { title, content } = JSON.parse(req.requestBody) as Partial<Entry>;
+
+    // Get current date
+    const now = new Date().toDateString();
+
+    const entry = diary.createEntry({
+      title,
+      content,
+      createdAt: now,
+      updatedAt: now,
+    });
+    diary.update({
+      ...diary.attrs,
+      updatedAt: now,
+    });
+    return {
+      diary: diary.attrs,
+      entry: entry.attrs,
+    };
+  } catch (error) {
+    return handleErrors(error, "Failed to save entry.");
+  }
+};
+
+// Get entries
+export const getEntries = (
+  schema: any,
+  req: Request
+): { entries: Entry[] } | Response => {
+  try {
+    // Find diary by params
+    const diary = schema.diaries.find(req.params.id);
+    return diary.entry;
+  } catch (error) {
+    return handleErrors(error, "Failed to get Diary entries.");
+  }
+};
+
+// Update an entry
+export const updateEntry = (schema: any, req: Request): Entry | Response => {
+  try {
+    // Find entry by params
+    const entry = schema.entries.find(req.params.id);
+
+    const data = JSON.parse(req.requestBody) as Partial<Entry>;
+
+    // Get current date
+    const now = new Date().toDateString();
+
+    entry.update({
+      ...data,
+      updatedAt: now,
+    });
+    return entry.attrs as Entry;
+  } catch (error) {
+    return handleErrors(error, "Failed to update entry.");
+  }
+};
+
+/* END ENTRY ROUTES */
